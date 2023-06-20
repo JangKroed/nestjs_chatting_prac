48a204affe409eda02a5802c8103d95ee80053e5
JangKroed <didlsdydgh@gmail.com>
2023-06-20 15:56:42 +0900

    nest-]chatting

---
+++ src/chats/chats.gateway.ts
@@ -0,0 +1,83 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Chatting } from './models/chattings.model';
+import { Model } from 'mongoose';
+import { Socket as SocketModel } from './models/sockets.model';
+
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  // 1
+  constructor(
+    @InjectModel(Chatting.name) private readonly chattingModel: Model<Chatting>,
+    @InjectModel(SocketModel.name)
+    private readonly socketModel: Model<SocketModel>,
+  ) {
+    this.logger.log('constructor');
+  }
+
+  // 4 - 연결 종료 시
+  async handleDisconnect(@ConnectedSocket() socket: Socket) {
+    const user = await this.socketModel.findOne({ id: socket.id });
+    if (user) {
+      socket.broadcast.emit('disconnect_user', user.username);
+      await user.deleteOne();
+    }
+    this.logger.log(`disconnected: ${socket.id}, ${socket.nsp.name}`);
+  }
+
+  // 3
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected: ${socket.id} ${socket.nsp.name}`);
+  }
+
+  // 2
+  afterInit() {
+    this.logger.log('init');
+  }
+
+  @SubscribeMessage('new_user')
+  async handleNewUser(
+    @MessageBody() username: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const exist = await this.socketModel.exists({ username });
+    if (exist)
+      await this.socketModel.create({
+        id: socket.id,
+        username: `${username}_${Math.floor(Math.random() * 100)}`,
+      });
+    else this.socketModel.create({ id: socket.id, username });
+
+    // username db에 적재
+    socket.broadcast.emit('user_connected', username);
+    return username;
+  }
+
+  @SubscribeMessage('submit_chat')
+  async handleSubmitChat(
+    @MessageBody() chat: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const socketObj = await this.socketModel.findOne({ id: socket.id });
+
+    await this.chattingModel.create({
+      user: socketObj,
+      chat,
+    });
+    socket.broadcast.emit('new_chat', { chat, username: socketObj.username });
+  }
+}
